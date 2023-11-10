c0b5a11fc73fffc9ac5a358934cdd2f8e5fde54b
godzzz <g0dzZz1lla@yandex.ru>
2023-11-10 17:35:52 +0400

    [v0.0.3] + AnimatorRef

---
+++ Runtime/AnimatorRef.cs
@@ -0,0 +1,12 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// © 2023 Nikolay Melnikov <n.melnikov@depra.org>
+
+using UnityEngine;
+
+namespace Depra.Ecs.Components
+{
+	public struct AnimatorRef
+	{
+		public Animator Value;
+	}
+}
