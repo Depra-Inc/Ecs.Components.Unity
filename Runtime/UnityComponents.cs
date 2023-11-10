6caafbd02f465b5db55f9d1550c49d01d6ab1e63
godzzz <g0dzZz1lla@yandex.ru>
2023-11-10 21:00:14 +0400

    [v0.0.5] + LineRendererRef

--- Runtime/UnityComponents.cs
+++ Runtime/UnityComponents.cs
@@ -10,6 +10,7 @@ namespace Depra.Ecs.Components
 		public ComponentPool<CameraRef> Cameras { get; private set; }
 		public ComponentPool<AnimatorRef> Animators { get; private set; }
 		public ComponentPool<RigidbodyRef> Rigidbodies { get; private set; }
+		public ComponentPool<LineRendererRef> LineRenderers { get; private set; }
 		public ComponentPool<CharacterControllerRef> CharacterControllers { get; private set; }
 
 		void IWorldRegistry.Initialize(World world)
@@ -18,6 +19,7 @@ namespace Depra.Ecs.Components
 			world.AddPool(Cameras = new ComponentPool<CameraRef>());
 			world.AddPool(Animators = new ComponentPool<AnimatorRef>());
 			world.AddPool(Rigidbodies = new ComponentPool<RigidbodyRef>());
+			world.AddPool(LineRenderers = new ComponentPool<LineRendererRef>());
 			world.AddPool(CharacterControllers = new ComponentPool<CharacterControllerRef>());
 		}
 
