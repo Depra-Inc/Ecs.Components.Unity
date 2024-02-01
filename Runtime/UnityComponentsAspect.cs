956e3ba770da475ec380f41c23c434a23dcb0918
godzzz <g0dzZz1lla@yandex.ru>
2024-02-02 02:20:25 +0400

    [v0.1.3] Some components moved to other modules

--- Runtime/UnityComponentsAspect.cs
+++ Runtime/UnityComponentsAspect.cs
@@ -8,25 +8,17 @@ namespace Depra.Ecs.Components
 	public sealed class UnityComponentsAspect : IComponentAspect
 	{
 		public ComponentPool<CameraRef> Cameras { get; private set; }
-		public ComponentPool<SliderRef> Sliders { get; private set; }
 		public ComponentPool<AnimatorRef> Animators { get; private set; }
 		public ComponentPool<TransformRef> Transforms { get; private set; }
-		public ComponentPool<RigidbodyRef> Rigidbodies { get; private set; }
 		public ComponentPool<LineRendererRef> LineRenderers { get; private set; }
-		public ComponentPool<RigidbodyForceMode> ForceModes { get; private set; }
-		public ComponentPool<CharacterControllerRef> CharacterControllers { get; private set; }
 
 		void IComponentAspect.Initialize(World world)
 		{
 			world.AddAspect(this);
 			world.AddPool(Cameras = new ComponentPool<CameraRef>());
-			world.AddPool(Sliders = new ComponentPool<SliderRef>());
 			world.AddPool(Animators = new ComponentPool<AnimatorRef>());
 			world.AddPool(Transforms = new ComponentPool<TransformRef>());
-			world.AddPool(Rigidbodies = new ComponentPool<RigidbodyRef>());
 			world.AddPool(LineRenderers = new ComponentPool<LineRendererRef>());
-			world.AddPool(ForceModes = new ComponentPool<RigidbodyForceMode>());
-			world.AddPool(CharacterControllers = new ComponentPool<CharacterControllerRef>());
 		}
 
 		void IComponentAspect.PostInitialize() { }
